32d8ca2db49c2850e9a9699e7f43ce3d5a553a13
Noralba Zafra <znoralba@gmail.com>
2022-04-11 11:24:46 -0400

    Docs Agregado

---
+++ docs/precache-manifest.f7ce60e1398d5a084b3abce32400d19d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc84480dec1ea6b702ab",
+    "url": "/static/css/main.67b7ddfb.chunk.css"
+  },
+  {
+    "revision": "dc84480dec1ea6b702ab",
+    "url": "/static/js/main.dc84480d.chunk.js"
+  },
+  {
+    "revision": "924f7fc64098af9dec0e",
+    "url": "/static/js/1.924f7fc6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "057fa36a3864ce243ba77797c9e0848c",
+    "url": "/index.html"
+  }
+];
